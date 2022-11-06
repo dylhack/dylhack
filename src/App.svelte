8e15551aadf1a29c517698f05606ac99b6f7bdd4
Dylan Hackworth <dylhack@gmail.com>
2022-11-05 20:58:18 -0500

    patch image metatags

--- src/App.svelte
+++ src/App.svelte
@@ -75,13 +75,13 @@
   <meta property="og:url" content="https://{hostName}">
   <meta property="og:title" content="{title}">
   <meta property="og:description" content="{description}">
-  <meta property="og:image" content="{image}">
+  <meta property="og:image" content="https://${hostName}/{image}">
   <!-- Twitter -->
   <meta property="twitter:card" content="summary_small_image">
   <meta property="twitter:url" content="https://{hostName}">
   <meta property="twitter:title" content="{title}">
   <meta property="twitter:description" content="{description}">
-  <meta property="twitter:image" content="{image}">
+  <meta property="twitter:image" content="https://${hostName}/{image}">
   <title>{hostName}</title>
 </svelte:head>
 
